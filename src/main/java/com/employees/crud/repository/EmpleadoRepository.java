ae9eb16c5c8ad9c607e93004359df73f809a138e
Ivan <balmacedaivan5@gmail.com>
2024-03-17 14:11:45 -0300

    empleado JPArepository

--- src/main/java/com/employees/crud/repository/EmpleadoRepository.java
+++ src/main/java/com/employees/crud/repository/EmpleadoRepository.java
@@ -1,2 +1,10 @@
-package com.employees.crud.repository;public interface EmpleadoRepository {
+package com.employees.crud.repository;
+
+import Model.empleado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmpleadoRepository extends JpaRepository<empleado, Long> {
+
 }
