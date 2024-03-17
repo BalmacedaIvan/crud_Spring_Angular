80b33b4c4d7aad672619e65aaacdeb4a5889b09f
Ivan <balmacedaivan5@gmail.com>
2024-03-17 14:10:47 -0300

    model package and entity

---
+++ src/main/java/Model/empleado.java
@@ -0,0 +1,60 @@
+package Model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="empleado")
+public class empleado {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="nombre", length = 60, nullable = false)
+    private String name;
+    @Column(name="apellido", length = 60, nullable = false)
+    private String lastname;
+    @Column(name="email", length = 60, nullable = false, unique = true)
+    private String email;
+
+    public empleado() {
+    }
+
+    public empleado(Long id, String name, String lastname, String email) {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/employees/crud/repository/EmpleadoRepository.java
@@ -0,0 +1,2 @@
+package com.employees.crud.repository;public interface EmpleadoRepository {
+}
